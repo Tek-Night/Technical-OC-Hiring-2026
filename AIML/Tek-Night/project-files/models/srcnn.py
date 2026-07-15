d606cd47e36da7097bc64f1e706bbea4109ccba1
Tek-Night <paarthshukla058@gmail.com>
2026-07-15 03:01:40 -0700

    Added srcnn class

---
+++ AIML/Tek-Night/project-files/models/srcnn.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+
+class SRCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(
+            in_channels= 3,
+            out_channels= 64,
+            kernel_size= 9,
+            padding= 4
+        )
+
+        self.conv2 = nn.Conv2d(
+            in_channels= 64,
+            out_channels= 32,
+            kernel_size= 1
+            # no padding needed
+        )
+
+        self.conv3 = nn.Conv2d(
+            in_channels= 32,
+            out_channels= 3,
+            kernel_size= 5,
+            padding= 2
+        )
+
+        self.relu = nn.ReLU(inplace= True)
+
+    def forward(self, x):
+
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.conv3(x)
+
+        return x
